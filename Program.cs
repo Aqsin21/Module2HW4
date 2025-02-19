673428178c40f3070aaeed3ea2dd63279d09d624
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-02-19 14:10:00 +0400

    'first'

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task1 
+            Func<double, double> areaCircle = radius => Math.PI * radius * radius;
+            Func<double, double, double> areaRectangle = (width, lenght) => width * lenght;
+            Func<double, double, double> areaTriangle = (baselenght, height) => 0.5 * baselenght * height;
+
+            Console.WriteLine($"Circle Area (R=5);{areaCircle(5)}");
+            Console.WriteLine($"Rectangle Area (w=6,h=8)={areaRectangle(6, 8)}");
+            Console.WriteLine($"Triangle Area (b=4 ,h=3)={areaTriangle(7, 3)}");
+
+        }
+    }
+}
