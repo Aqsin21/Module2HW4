ad6d9a57ccbb7193397b78f1e1d538dc615569e1
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-02-19 17:25:57 +0400

    second'

--- Program.cs
+++ Program.cs
@@ -1,18 +1,72 @@
-﻿namespace Task4
+﻿using System.Runtime.CompilerServices;
+
+namespace Task4
 {
     internal class Program
     {
+        public delegate void Method();
         static void Main(string[] args)
         {
-            // Task1 
+            // Task 1
             Func<double, double> areaCircle = radius => Math.PI * radius * radius;
             Func<double, double, double> areaRectangle = (width, lenght) => width * lenght;
-            Func<double, double, double> areaTriangle = (baselenght, height) => 0.5 * baselenght * height;
+            Func<double, double, double> areaTriangle = (baselenght, height) => ( baselenght * height)/2;
 
             Console.WriteLine($"Circle Area (R=5);{areaCircle(5)}");
             Console.WriteLine($"Rectangle Area (w=6,h=8)={areaRectangle(6, 8)}");
             Console.WriteLine($"Triangle Area (b=4 ,h=3)={areaTriangle(7, 3)}");
 
+            // Task 2
+
+            Action multicatsDelegate = Method1;
+            multicatsDelegate += Method2;
+            multicatsDelegate += Method3;
+
+            multicatsDelegate.Invoke();
+
+
+            // Task 3   
+
+            List<string> names = new List<string> { "Aqsin","Yaqub","Metin","Malik"};
+            Func<string, bool> startsWithA = name => name.StartsWith("A");
+            var filteredNames = names.Where(startsWithA);
+
+            Console.WriteLine("\nNames starting with 'A':");
+            foreach (var name in filteredNames)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
+
+
+
+
+
+
+            // Task 4
+            Func<int, int, int> add = (x, y) => x + y;
+            Func<int ,int ,int> sub= (x, y) => x - y;
+            Func<int, int, int> mult = (x, y) => x *y;
+            Func<int, int, double> divide = (x, y) => y != 0 ? (double)x / y : throw new DivideByZeroException("Cannot divide by zero");
+            Console.WriteLine($" Answer{ add(4,6)}");
+            Console.WriteLine($" Answer{sub (9, 5)}");
+            Console.WriteLine($" Answer{mult(4, 6)}");
+            Console.WriteLine($" Answer{divide(9, 3)}");
+
+        }
+        public static void Method1()
+        {
+            Console.WriteLine("Method is true");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method is true");
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Method is true");
         }
     }
 }
--- obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673428178c40f3070aaeed3ea2dd63279d09d624")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
